da8a6fe426fd43193b0cb8ff8255f439779c4c30
ifrah-ashraf <137814364+ifrah-ashraf@users.noreply.github.com>
2024-01-02 23:40:11 +0530

    solution for 4-promise-chain.js

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,40 @@
  */
 
 function wait1(t) {
-
+ return new Promise((resolve)=>{
+  setTimeout(()=>{
+    resolve(t) 
+  },t)
+ })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(t)
+        },t)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(t)
+        },t)
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const startTime=Date.now() 
+    return wait1(t1*1000)
+    .then(()=>wait2(t2*1000))
+    .then(()=>wait3(t3*1000))
+    .then(()=>{
+        const endTime=Date.now()
+        return endTime-startTime
+    })
 }
+
+  
 
 module.exports = calculateTime;
